bf2f12de1af9934eaceab68ec089086b5a76cd1c
mattdood <mawimberly@csumb.edu>
2020-06-12 18:11:27 -0700

    finished scraping project

--- scraping.py
+++ scraping.py
@@ -2,12 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+def save_function(article_list):
+    with open('articles.txt', 'w') as outfile:
+        json.dump(article_list, outfile)
+
 def hackernews_rss():
+    article_list = []
+
     try:
         r = requests.get('https://news.ycombinator.com/rss')
         soup = BeautifulSoup(r.content, features='xml')
 
-        return print(soup)
+        articles = soup.findAll('item')
+
+        for a in articles:
+            title = a.find('title').text
+            link = a.find('link').text
+            published = a.find('pubDate').text
+            article = {
+                'title': title,
+                'link': link,
+                'published': published
+                }
+            article_list.append(article)
+        return save_function(article_list)
     except Exception as e:
         print('The scraping job failed. See exception:')
         print(e)
